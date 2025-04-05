c2b959d2e3bc8e7bc48a8bcc877fd1a12116cc83
Elissa-DI <elissadusabe@gmail.com>
2025-04-05 16:22:07 +0200

    expanded widgets added

--- myapp_1/lib/main.dart
+++ myapp_1/lib/main.dart
@@ -1,21 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(MaterialApp(home: Home()));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text('My App_1'),
+        title: Text(
+          'My App',
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white70),
+        ),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
-      body: Center(
-        child: Text('Hello this is the body'),
+      body: Row(
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: Image.asset('assets/ghost.png'),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              color: Colors.cyan,
+              child: Text('1'),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              color: Colors.red,
+              child: Text('2'),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              color: Colors.amber,
+              child: Text('3'),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print("Floating Action Button Clicked!");
-        },
-        child: Text('Click'),
+        onPressed: () {},
+        backgroundColor: Colors.red[500],
+        child: Text('Click me'),
       ),
-    ),
-  ));
+    );
+  }
 }
